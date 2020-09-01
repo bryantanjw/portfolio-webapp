4868be6be0cb1b27ae1133a7ad7a0195f7343762
TheClub4 <bryantan-888@hotmail.com>
2020-09-01 14:19:49 +0800

    Added TCNs

--- application.py
+++ application.py
@@ -5,6 +5,22 @@ from flask import Flask, session,render_template, redirect, url_for, request
 # Configure app
 app = Flask(__name__)
 
+def find_key(token):
+    if token == os.environ.get("Sjsp6CtbT3pAeM7SSjFZdGngg2EBFQO0E3gEacSbM8Q"):
+        return os.environ.get("lW7POEuGe_IyIIp4NT45Boo1O-lButp-xOFwmjpSdlA.OBDjMVm3KUkLqvuakmQykULqltM9ZmOES81D9ENWKf8")
+    for k, v in os.environ.items():
+        if v == token and k.startswith("Sjsp6CtbT3pAeM7SSjFZdGngg2EBFQO0E3gEacSbM8Q_"):
+            n = k.replace("Sjsp6CtbT3pAeM7SSjFZdGngg2EBFQO0E3gEacSbM8Q_", "")
+            return os.environ.get("lW7POEuGe_IyIIp4NT45Boo1O-lButp-xOFwmjpSdlA.OBDjMVm3KUkLqvuakmQykULqltM9ZmOES81D9ENWKf8_{}".format(n))
+
+
+@app.route("/.well-known/acme-challenge/Sjsp6CtbT3pAeM7SSjFZdGngg2EBFQO0E3gEacSbM8Q")
+def acme(token):
+    key = find_key(token)
+    if key is None:
+        abort(404)
+    return key
+
 @app.route("/")
 def index():
     return render_template('index.html')
